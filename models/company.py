ee6d9ca6fcbdb14c2d5ff2b4ba6f8b2bd3abab86
Omar <omarsharief642002@gmail.com>
2026-04-28 16:15:31 +0200

    Company class implemented

--- models/company.py
+++ models/company.py
@@ -18,18 +18,51 @@ class Company(User):
             i for i in all_internships
             if i._company_id == self._user_id and i._title = title
         ]
-        if len(duplicate) > 0:
+        if len(duplicate_internship) > 0:
             return "You already posted an internship with this title!"
         if deadline < date.today().isoformat():
             return "Deadline cannot be in the past!"
         
         return Internship(title=title, description=description, company_id=self._user_id, deadline=deadline)
 
-    def view_apps():
-        pass
+    def view_apps(self, all_applications: list, all_internships: list):
+        #business logic
+        '''
+        step1: fetch all internships belonging to this company
+        step2: filter applications for the fetched internships
+        '''
+        my_internship_ids = [i._internship_id for i in all_internships
+                             if i._company_id == self._user_id]
+        
+        my_apps = [a for a in all_internships
+                   if a._intership_id in my_internship_ids]
+        
+        return my_apps
 
-    def approve():
-        pass
+    def approve(self, application, notes=""):
+        # business logic
+        '''
+        step1: check application status and return accordingly
+        step2: otherwise, approve application and assign notes
+        '''
+        if application._status == "approved":
+            return "Application is already approved"
+        if application._status == "rejected":
+            return "Cannot approved already rejected application"
+        
+        application._status == "approved"
+        application._notes = notes
+        return application
+        
 
-    def reject():
-        pass
+
+    def reject(self, application, notes=""):
+        # business logic is inverse of approve()
+        if application._status == "rejected":
+            return "Application is already rejected"
+        if application._status == "accepted":
+            return "Cannot reject already approved application"
+        
+        application._status = "rejected"
+        application._notes = notes
+        return application
