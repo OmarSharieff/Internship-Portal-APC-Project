6fc8ab25e6820dbd6b6806b9c8049fa45ec51026
Omar <omarsharief642002@gmail.com>
2026-04-28 15:45:09 +0200

    Add post_intern method

--- models/company.py
+++ models/company.py
@@ -1,4 +1,6 @@
 from user import User
+from internship import Internship
+from datetime import date
 
 class Company(User):
     def __init__(self, user_id, name, email, password, industry, location):
@@ -6,8 +8,22 @@ class Company(User):
         self._industry = industry
         self._location = location
 
-    def post_intern():
-        pass
+    def post_intern(self, title, description, deadline, all_internships: list):
+        #business logic
+        '''
+        step1: check for already existing internship and valid deadline
+        step2: otherwise, post an internship
+        '''
+        duplicate_internship = [
+            i for i in all_internships
+            if i._company_id == self._user_id and i._title = title
+        ]
+        if len(duplicate) > 0:
+            return "You already posted an internship with this title!"
+        if deadline < date.today().isoformat():
+            return "Deadline cannot be in the past!"
+        
+        return Internship(title=title, description=description, company_id=self._user_id, deadline=deadline)
 
     def view_apps():
         pass
