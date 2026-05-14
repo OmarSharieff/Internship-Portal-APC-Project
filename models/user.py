81e304b5275e99ac044853accdf061b66b9389cd
Syed-Fahad-Ali-27 <syed.fahad.ali2704@gmail.com>
2026-05-14 18:14:51 +0200

    True Abstract Class

--- models/user.py
+++ models/user.py
@@ -1,12 +1,22 @@
-# This is an abstract base class
-from abc import ABC
+from abc import ABC, abstractmethod
+
+
 class User(ABC):
     def __init__(self, user_id, name, email, password):
         self._user_id = user_id
         self._name = name
-        self._email = email
+        self._email = email.lower()
         self._password = password
-    
-    # currently assuming login logic is same for all kinds of users (Student, Company, Instructor)
+
     def login(self, email, password):
-        return self._email == email and self._password == password
+        return self._email == email.lower() and self._password == password
+
+    def get_user_id(self):
+        return self._user_id
+
+    def get_name(self):
+        return self._name
+
+    @abstractmethod
+    def __str__(self):
+        pass
