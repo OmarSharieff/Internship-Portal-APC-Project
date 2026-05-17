545a96d6a75f6af102ae4e8788dc96f8e0d39986
Omar <omarsharief642002@gmail.com>
2026-05-17 11:55:32 +0200

    use any() method instead of len()

--- models/student.py
+++ models/student.py
@@ -10,27 +10,27 @@ class Student(User):
         self._resume = resume
 
     def view_intern(self, all_internships: list):
-        # This business logic code is written prior to sqlite3 schema setup assuming Internship class to have deadline and 'is_open' attributes.
-        # NOTE: isoformat() is used for comparing date. Do not change the format otherwise it will break the logic!
+        # returns only open internsips which haven't passed their deadline
+        # NOTE: isoformat() is used for comparing dates as strings. Do not change the format otherwise it will break the logic!
         return [i for i in all_internships if i._is_open and i._deadline >= date.today().isoformat()]
         
     def apply(self, internship, all_applications: list):
-        already_applied = [
-            a for a in all_applications
-            if a._student_id == self._user_id and a._internship_id == internship._internship_id
-        ]
-        if len(already_applied) > 0:
+
+
+        already_applied = any(a._student_id == self._user_id and a._internship_id == internship._internship_id for a in all_applications)
+        
+        if already_applied:
             return "Already applied to this internship."
         if not internship._is_open:
             return "This internship is closed."
         if internship._deadline < date.today().isoformat():
             return "Deadline has passed."
 
-        #TODO: app_id=None is only for test purposes. Remove this when database is integrated
+        #NOTE: app_id=None is only for test purposes.
         return Application(app_id=None, student_id=self._user_id, internship_id=internship._internship_id)
         
 
     def view_apps(self, all_applications: list):
-        # same logic
+        # filtering applications that belong to a particular student (sorted by most recent)
         my_apps= [a for a in all_applications if a._student_id == self._user_id]
         return sorted(my_apps, key=lambda a: a._applied_at, reverse=True) #ordering by recently applied
